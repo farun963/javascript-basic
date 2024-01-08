f69e7681fceb2bb4737effe11413b4adaf962f11
farun963 <gerar963@gmail.com>
2024-01-08 20:56:05 -0300

    commit laptop

---
+++ valores.js
@@ -0,0 +1,26 @@
+// Ejemplo de objeto
+let persona = {
+    nombre: "Juan",
+    edad: 30,
+    ciudad: "Madrid"
+};
+  
+// Acceder a los valores del objeto
+console.log(persona.nombre); // Imprime: Juan
+console.log(persona.edad); // Imprime: 30
+console.log(persona.ciudad); // Imprime: Madrid
+
+  
+// Ejemplo de array
+let frutas = ["Manzana", "Plátano", "Fresa", "Naranja"];
+
+// Acceder a los elementos del array
+console.log(frutas[0]); // Imprime: Manzana
+console.log(frutas[2]); // Imprime: Fresa
+
+// Añadir un elemento al final del array
+frutas.push("Pera");
+console.log(frutas); // Imprime: ["Manzana", "Plátano", "Fresa", "Naranja", "Pera"]
+
+// Longitud del array
+console.log(frutas.length); // Imprime: 5
