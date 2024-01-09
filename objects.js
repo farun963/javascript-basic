5d71b10b76c10524d1a715f39882660f60348ce1
farun963 <gerar963@gmail.com>
2024-01-08 22:11:48 -0300

    commits

---
+++ objects.js
@@ -0,0 +1,15 @@
+var objeto = {};
+
+var miAuto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    annio: 2020,
+    detalleDelAuto: function(){
+        console.log(`Auto ${this.modelo} ${this.annio}`)
+    }
+};
+
+
+miAuto.marca;
+
+miAuto.detalleDelAuto();
--- while.js
+++ while.js
@@ -1,16 +1,16 @@
 var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
 
-funcion saludarEstudiantes(estudiante){
-    console.log(`Hola, ${estudiante}`)
+function saludarEstudiantes(estudiante){
+    console.log(`Hola, ${estudiante}`);
 }
 
 while(estudiantes.length > 0){
     var estudiante = estudiantes.shift();
-    saludarEstudiantes(estudiante)
+    saludarEstudiantes(estudiante);
 }
 
 while(estudiantes.length > 0){
     console.log(estudiantes);
     var estudiante = estudiantes.shift();
-    saludarEstudiantes(estudiante)
+    saludarEstudiantes(estudiante);
 }
