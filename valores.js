0e5a66df98f0685e9e6e84790409ff3fb3612b6f
farun963 <gerar963@gmail.com>
2024-01-08 23:05:42 -0300

    last one, promise.

--- valores.js
+++ valores.js
@@ -1,26 +1,22 @@
-// Ejemplo de objeto
-let persona = {
-    nombre: "Juan",
-    edad: 30,
-    ciudad: "Madrid"
-};
-  
-// Acceder a los valores del objeto
-console.log(persona.nombre); // Imprime: Juan
-console.log(persona.edad); // Imprime: 30
-console.log(persona.ciudad); // Imprime: Madrid
+/*
 
-  
-// Ejemplo de array
-let frutas = ["Manzana", "Plátano", "Fresa", "Naranja"];
+Cheatsheet de Valores en JavaScript
+Valores Truthy:
 
-// Acceder a los elementos del array
-console.log(frutas[0]); // Imprime: Manzana
-console.log(frutas[2]); // Imprime: Fresa
+    true
+    Cualquier string no vacío ('hola', 'verdadero', etc.)
+    Cualquier número distinto de 0 (42, -1, 3.14, etc.)
+    Objetos ({}, {'key': 'value'}, new Date(), etc.)
+    Arrays ([], [1, 2, 3], etc.)
+    Funciones (function() {}, (x) => x * 2, etc.)
 
-// Añadir un elemento al final del array
-frutas.push("Pera");
-console.log(frutas); // Imprime: ["Manzana", "Plátano", "Fresa", "Naranja", "Pera"]
+Valores Falsy:
 
-// Longitud del array
-console.log(frutas.length); // Imprime: 5
+    false
+    String vacío ('')
+    0 (número cero)
+    null
+    undefined
+    NaN (Not a Number)
+
+    */
---
+++ variables.js
@@ -0,0 +1,11 @@
+// Declaración de variables con var (antes de ES6)
+var nombre = 'John';
+var edad = 25;
+
+// Declaración de variables con let (preferida para variables cambiantes)
+let saldo = 100;
+let mensaje = 'Hola, mundo';
+
+// Declaración de variables constantes con const (inmutable)
+const PI = 3.1416;
+const URL_API = 'https://api.ejemplo.com';
